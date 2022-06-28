d8dd72970de01ddf78848e67ce95d7c332b2122b
Gustavo Jimenez <gjimenez.code@gmail.com>
2022-06-28 15:35:27 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './enum/response-status.enum';
 export * from './interceptor/timeout.interceptor';
-export * from './interceptor/http-exception.filter';
 export * from './dto/response.dto';
 export * from './dto/result.dto';
