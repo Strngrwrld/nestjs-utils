44ed8c9798f9e1c008faffa54776b667b9e2d5fe
Gustavo Jimenez <gjimenez.code@gmail.com>
2022-06-28 15:11:13 -0500

    implementa http filter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './enum/response-status.enum';
 export * from './interceptor/timeout.interceptor';
+export * from './interceptor/http-exception.filter';
 export * from './dto/response.dto';
 export * from './dto/result.dto';
