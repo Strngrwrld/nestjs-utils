882a7e0df551e13fdff00afa6d255a3f6bfd6d4b
Gustavo Jimenez <gjimenez.code@gmail.com>
2022-06-28 14:58:20 -0500

    implement timeoutException

---
+++ src/interceptor/timeout.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  RequestTimeoutException,
+} from '@nestjs/common';
+import {
+  catchError,
+  Observable,
+  throwError,
+  timeout,
+  TimeoutError,
+} from 'rxjs';
+
+export class TimeoutInterceptor implements NestInterceptor {
+  static TIMEOUT_RESPONSE_TIME_MS = 5000;
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      timeout(TimeoutInterceptor.TIMEOUT_RESPONSE_TIME_MS),
+      catchError((err) => {
+        if (err instanceof TimeoutError) {
+          return throwError(() => new RequestTimeoutException());
+        }
+        return throwError(() => err);
+      }),
+    );
+  }
+}
